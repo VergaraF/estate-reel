0d9a0c28dcf1f8066928a360b5b4929ab05db753
Ajmer Singh Gadreh <ajmersingh_gadreh@hotmail.com>
2014-11-26 15:41:37 -0500

    showing details of products, displaying owner profile

--- showDetails.php
+++ showDetails.php
@@ -8,9 +8,47 @@
 	include('PreCode/header.php');
 	require('PreCode/authentication.php');
 ?>
+	<form name="viewOwnerProfile" method="POST" action="">
+		<input name="viewProfile" type="submit" value="View Owner Profile" >
+	</form>
 	<div id="col1">
 		<div id="gallery">
 			<?php
+				if (isset($_POST['viewProfile'])) {
+			?>		
+					<h2>Owner Info</h2>
+					<table align="center" border="1">
+						<tbody>
+			<?php	
+					$user_id = null;
+					$resultSetArray = $productObj->displaySpecificProduct($_GET['hiddenID']);
+					if (count($resultSetArray) === 1) {
+						$user_id = $resultSetArray[0]['user_id'];
+						$userInfoArray = $loginObj->getUserById($user_id);
+						for ($row=0; $row < count($userInfoArray); $row++) { 
+			?>
+							<tr>
+								<td>First Name</td>
+								<td> <?php echo $userInfoArray[$row]['firstname']; ?> </td>
+							</tr>
+							<tr>
+								<td>Last Name</td>
+								<td> <?php echo $userInfoArray[$row]['lastname']; ?> </td>
+							</tr>
+							<tr>
+								<td>Phone Number</td>
+								<td> <?php echo $userInfoArray[$row]['phoneNumber']; ?> </td>
+							</tr>
+			<?php 	
+						} 
+					}else{
+						echo "Invalid input";
+					}
+			?>
+						</tbody>
+					</table><br>
+			<?php	
+				}
 				$databaseObj->createConnection();
 				$allTheImages = $productObj->getAllTheImages($_GET['hiddenID']);
 				for ($row = 0; $row < count($allTheImages); $row++) {
@@ -20,19 +58,79 @@
 			<span class="span" id="0" ></span>
 		</div>
 		<img class="arrow" src="apartment_images/left.png" onClick="return goLeft(document.getElementsByClassName('span'))">
-		<img class="bigImage" src="apartment_images/<?php echo $allTheImages[0]['file_name']; ?>">
+		<img class="bigImage" src="apartment_images/
+			<?php 
+				if(strcmp($allTheImages[0]['file_name'], '') === 0){
+					echo 'no_image.jpg';
+				}else{
+					echo $allTheImages[0]['file_name'];
+				}
+			?>">
 		<img class="arrow" src="apartment_images/right.png" onClick="return goRight(document.getElementsByClassName('span'))">
 	</div>
 <div id="col2">
 <h2>Detailed Info</h2>
+<table id="details" align="center" border="1">
+	<tbody>
 <?php
 	$detailedInfo = $productObj->displaySpecificProduct($_GET['hiddenID']);
 	for ($row = 0; $row < count($detailedInfo); $row++){
-		echo "<p>Address: " . $detailedInfo[$row]['house_no'] . " "  . $detailedInfo[$row]['street_name'] . "<br>" 
-							. $detailedInfo[$row]['city'] 	  . ", " . $detailedInfo[$row]['province'] 	  . "<br>" 
-							. $detailedInfo[$row]['zip_code'] . ", " . $detailedInfo[$row]['country'] 	  . "</p>";
-	}
 ?>
+		<tr>
+			<td>House Number</td>
+			<td> <?php echo $detailedInfo[$row]['house_no']; ?> </td>
+		</tr>
+		<tr>
+			<td>Street Name</td>
+			<td> <?php echo $detailedInfo[$row]['street_name']; ?> </td>
+		</tr>
+		<tr>
+			<td>City</td>
+			<td> <?php echo $detailedInfo[$row]['city']; ?> </td>
+		</tr>
+		<tr>
+			<td>Province</td>
+			<td> <?php echo $detailedInfo[$row]['province']; ?> </td>
+		</tr>
+		<tr>
+			<td>Zip Code</td>
+			<td> <?php echo $detailedInfo[$row]['zip_code']; ?> </td>
+		</tr>
+		<tr>
+			<td>Country</td>
+			<td> <?php echo $detailedInfo[$row]['country']; ?> </td>
+		</tr>
+		<tr>
+			<td>Type</td>
+			<td> <?php echo $detailedInfo[$row]['type']; ?> </td>
+		</tr>
+		<tr>
+			<td>Description</td>
+			<td> <?php echo $detailedInfo[$row]['description']; ?> </td>
+		</tr>
+		<tr>
+			<td>Rooms no.</td>
+			<td> <?php echo $detailedInfo[$row]['no_of_rooms']; ?> </td>
+		</tr>
+		<tr>
+			<td>Bathrooms no.</td>
+			<td> <?php echo $detailedInfo[$row]['no_of_bathrooms']; ?> </td>
+		</tr>
+		<tr>
+			<td>Living no.</td>
+			<td> <?php echo $detailedInfo[$row]['no_of_living_rooms']; ?> </td>
+		</tr>
+		<tr>
+			<td>Price</td>
+			<td> <?php echo $detailedInfo[$row]['price']; ?> </td>
+		</tr>
+		<tr>
+			<td>It is for </td>
+			<td> <?php echo $detailedInfo[$row]['rangeType']; ?> </td>
+		</tr>
+		</tbody>
+<?php } ?>
+</table>
 </div>
 </section>
 <script>
